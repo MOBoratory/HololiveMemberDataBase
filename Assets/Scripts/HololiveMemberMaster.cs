7ed0829b734c1d88160d77e3d13b8a68933991e0
ChapaCactus <moboratory@gmail.com>
2022-07-17 18:05:27 +0900

    Date

--- Assets/Scripts/HololiveMemberMaster.cs
+++ Assets/Scripts/HololiveMemberMaster.cs
@@ -1,3 +1,4 @@
+using System;
 using MOB.ScriptableMasterSystem;
 using UnityEngine;
 
@@ -8,14 +9,25 @@ namespace MOB.HololiveMember.Master
     /// </summary>
     public sealed class HololiveMemberMaster : MasterBase
     {
+        // 名称
         [SerializeField] private string _characterName;
         [SerializeField] private string _nickName;
+
+        // 日付
+        [SerializeField] private string _birthDay;
+        [SerializeField] private string _debutDate;
 
         /// <summary>キャラクター名称</summary>
         public string CharacterName => _characterName;
 
         /// <summary>愛称</summary>
         public string NickName => _nickName;
+
+        /// <summary>誕生日</summary>
+        public DateTime BirthDay => DateTime.Parse(_birthDay);
+
+        /// <summary>デビュー日付</summary>
+        public DateTime DebutDate => DateTime.Parse(_debutDate);
 
         /// <summary>
         ///     IDでマスタを取得
