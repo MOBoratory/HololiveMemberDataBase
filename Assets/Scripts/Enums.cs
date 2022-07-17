c1233df8288da0511cf6444352d774ce8ae1fdd7
ChapaCactus <moboratory@gmail.com>
2022-07-17 22:27:02 +0900

    Generations

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,24 @@
+namespace MOB.HololiveMember.Enums
+{
+    /// <summary>言語</summary>
+    public enum Languages
+    {
+        JP = 0,
+        EN = 1,
+        ID = 2
+    }
+
+    /// <summary>世代</summary>
+    public enum Generations
+    {
+        Gen0 = 0,
+        Gen1 = 1,
+        Gen2 = 2,
+        Gen3 = 3,
+        Gen4 = 4,
+        Gen5 = 5,
+        Gen6 = 6,
+
+        Gamers = 7
+    }
+}
--- Assets/Scripts/HololiveMemberMaster.cs
+++ Assets/Scripts/HololiveMemberMaster.cs
@@ -1,4 +1,5 @@
 using System;
+using MOB.HololiveMember.Enums;
 using MOB.ScriptableMasterSystem;
 using UnityEngine;
 
@@ -13,10 +14,15 @@ namespace MOB.HololiveMember.Master
         // 名称
         [SerializeField] private string _characterName;
         [SerializeField] private string _nickName;
+        [SerializeField] private string _fanName;
 
         // 日付
         [SerializeField] private string _birthDay;
         [SerializeField] private string _debutDate;
+
+        [SerializeField] private Languages _languages;
+        [SerializeField] private Generations _generation;
+        [SerializeField] private string _description;
 
         /// <summary>キャラクター名称</summary>
         public string CharacterName => _characterName;
@@ -24,11 +30,23 @@ namespace MOB.HololiveMember.Master
         /// <summary>愛称</summary>
         public string NickName => _nickName;
 
+        /// <summary>ファン名称</summary>
+        public string FanName => _fanName;
+
         /// <summary>誕生日</summary>
         public DateTime BirthDay => DateTime.Parse(_birthDay);
 
         /// <summary>デビュー日付</summary>
         public DateTime DebutDate => DateTime.Parse(_debutDate);
+
+        /// <summary>言語</summary>
+        public Languages Languages => _languages;
+
+        /// <summary>世代</summary>
+        public Generations Generation => _generation;
+
+        /// <summary>説明文</summary>
+        public string Description => _description;
 
         /// <summary>
         ///     IDでマスタを取得
