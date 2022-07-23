8d002e31c714e9d40b291ced60a94d4b3a2713e5
MOBoratory <moboratory@gmail.com>
2022-07-24 00:39:51 +0900

    準備

--- Assets/MOB/HololiveMember/Master/HololiveMemberGroupMaster.cs
+++ Assets/MOB/HololiveMember/Master/HololiveMemberGroupMaster.cs
@@ -1,27 +1,33 @@
+using System.Collections.Generic;
 using MOB.ScriptableMasterSystem;
 using UnityEngine;
 
 namespace MOB.HololiveMember.Master
 {
     /// <summary>
-    ///     ホロライブメンバーマスタ
+    ///     ホロライブメンバーグループマスタ
     /// </summary>
     [CreateAssetMenu(order = 1, fileName = "HololiveMemberGroupMaster", menuName = "MOB/HololiveMemberGroupMaster")]
     public sealed class HololiveMemberGroupMaster : MasterBase
     {
-        [SerializeField] private string _characterName;
+        [SerializeField] private string _groupName;
         [SerializeField] private string _description;
 
-        /// <summary>キャラクター名称</summary>
-        public string CharacterName => _characterName;
+        [SerializeField] private HololiveMemberMaster[] _memberMasters;
+
+        /// <summary>名称</summary>
+        public string GroupName => _groupName;
 
         /// <summary>説明文</summary>
         public string Description => _description;
 
+        /// <summary>所属メンバー</summary>
+        public IReadOnlyList<HololiveMemberMaster> MemberMasters => _memberMasters;
+
         /// <summary>
         ///     IDでマスタを取得
         /// </summary>
-        /// <param name="id">HololiveMemberMaster.ID</param>
+        /// <param name="id">HololiveMemberGroupMaster.ID</param>
         /// <returns>MasterInstance</returns>
         public static HololiveMemberGroupMaster GetById(int id)
         {
