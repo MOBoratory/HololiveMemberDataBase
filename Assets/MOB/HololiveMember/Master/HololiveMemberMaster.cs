f856a23cf12e11c631addd6bb569190a0c24b869
ChapaCactus <moboratory@gmail.com>
2022-09-05 01:08:37 +0900

    整理

--- Assets/MOB/HololiveMember/Master/HololiveMemberMaster.cs
+++ Assets/MOB/HololiveMember/Master/HololiveMemberMaster.cs
@@ -11,27 +11,16 @@ namespace MOB.HololiveMember.Master
     [CreateAssetMenu(order = 1, fileName = "HololiveMemberMaster", menuName = "MOB/HololiveMemberMaster")]
     public sealed partial class HololiveMemberMaster : MasterBase
     {
-        // 名称
         [SerializeField] private string _characterName;
-        [SerializeField] private string _nickName;
-        [SerializeField] private string _fanName;
 
-        // 日付
         [SerializeField] private string _birthDay;
         [SerializeField] private string _debutDate;
 
         [SerializeField] private Languages _languages;
         [SerializeField] private Generations _generation;
-        [SerializeField] private string _description;
 
         /// <summary>キャラクター名称</summary>
         public string CharacterName => _characterName;
-
-        /// <summary>愛称</summary>
-        public string NickName => _nickName;
-
-        /// <summary>ファン名称</summary>
-        public string FanName => _fanName;
 
         /// <summary>誕生日</summary>
         public DateTime BirthDay
@@ -63,11 +52,5 @@ namespace MOB.HololiveMember.Master
 
         /// <summary>言語</summary>
         public Languages Languages => _languages;
-
-        /// <summary>世代</summary>
-        public Generations Generation => _generation;
-
-        /// <summary>説明文</summary>
-        public string Description => _description;
     }
 }
