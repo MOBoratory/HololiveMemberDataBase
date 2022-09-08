21c84319c8d8e60bed7be2e40fe44570dbb216d6
ChapaCactus <moboratory@gmail.com>
2022-09-09 01:29:47 +0900

    JsonParser

---
+++ Assets/MOB/HololiveMember/Master/HololiveMemberMasterJsonParser.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MOB.HololiveMember.Master
+{
+    /// <summary>
+    ///     Json変換
+    /// </summary>
+    public sealed partial class HololiveMemberMasterJsonParser
+    {
+    }
+}
