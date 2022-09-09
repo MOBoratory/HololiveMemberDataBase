ed462fa96679a5b1766fa7e350970e5819640b76
Kunimoto.Takuya <kunimoto.takuya@donuts.ne.jp>
2022-09-09 13:56:08 +0900

    とりあえずのJson書き出し機能

--- Assets/MOB/HololiveMember/Master/HololiveMemberMasterJsonParser.cs
+++ Assets/MOB/HololiveMember/Master/HololiveMemberMasterJsonParser.cs
@@ -1,3 +1,8 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
 namespace MOB.HololiveMember.Master
 {
     /// <summary>
@@ -5,5 +10,33 @@ namespace MOB.HololiveMember.Master
     /// </summary>
     public sealed class HololiveMemberMasterJsonParser
     {
+        private const string ResourcesPath = "Assets/Resources/MasterDataJson";
+        private const string MasterRootDirectoryPath = "Assets/Resources/MasterData";
+
+        [MenuItem("MOB/Master/ConvertMasterObjectsToJson")]
+        private static void ExportJsons()
+        {
+            Debug.Log($"{nameof(ExportJsons)}");
+
+            var saveDirectoryPath = Path.Combine(Application.dataPath, $"Export");
+            if (!Directory.Exists(saveDirectoryPath))
+            {
+                Directory.CreateDirectory(saveDirectoryPath);
+            }
+
+            var masterObjects = LoadHololiveMemberMasterObjects();
+            foreach (var masterObject in masterObjects)
+            {
+                var json = JsonUtility.ToJson(masterObject);
+                var savePath = Path.Combine(saveDirectoryPath, $"{masterObject.name}.json");
+                Debug.Log($"{nameof(savePath)}: {savePath}");
+                File.WriteAllText(savePath, json);
+            }
+        }
+
+        private static HololiveMemberMaster[] LoadHololiveMemberMasterObjects()
+        {
+            return Resources.LoadAll<HololiveMemberMaster>("MasterData");
+        }
     }
 }
